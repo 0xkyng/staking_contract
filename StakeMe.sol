f52626f511548ee3e2998acfee572fcd8eb07b0b
Isaac <teryima127@gmail.com>
2023-09-04 23:11:33 +0100

    withdraw staked amount

--- StakeMe.sol
+++ StakeMe.sol
@@ -4,14 +4,12 @@ pragma solidity 0.8.19;
 import {IStandardToken} from "./IStandardToken.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract StakeMe is ERC20{
+contract StakeMe is ERC20 {
     IStandardToken standardToken;
     uint256 min_stake_duration = 1 minutes;
     uint8 rate = 3;
     uint8 percentage = 100;
     uint public contractBalance;
-    
-    
 
     struct User {
         uint amountStaked;
@@ -19,22 +17,26 @@ contract StakeMe is ERC20{
     }
     event Staked(uint amountStake, uint totalAmountStaked, uint time);
 
-    mapping (address => User) user;
+    mapping(address => User) user;
 
-    constructor(address _standardToken) ERC20("STAKING_REWARDS_TOKEN", "SRT"){
-       standardToken = IStandardToken(_standardToken);
+    constructor(address _standardToken) ERC20("STAKING_REWARDS_TOKEN", "SRT") {
+        standardToken = IStandardToken(_standardToken);
     }
 
     // Stake your standard tken
     function stake(uint amount) external {
-         uint balance = standardToken.balanceOf(msg.sender);
-         require(balance >= amount, "Insufficient balance");
-         bool success = standardToken.transferFrom(msg.sender, address(this), amount);
-         require(success, "transfer failed");
-         User storage staker = user[msg.sender];
-         staker.amountStaked+= amount;
-         staker.timeStaked = block.timestamp;
-         emit Staked(amount, staker.amountStaked, block.timestamp);
+        uint balance = standardToken.balanceOf(msg.sender);
+        require(balance >= amount, "Insufficient balance");
+        bool success = standardToken.transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        require(success, "transfer failed");
+        User storage staker = user[msg.sender];
+        staker.amountStaked += amount;
+        staker.timeStaked = block.timestamp;
+        emit Staked(amount, staker.amountStaked, block.timestamp);
     }
 
     // get the amount the user staked
@@ -44,11 +46,25 @@ contract StakeMe is ERC20{
     }
 
     // user can withraw staked token
-    function withdraw(uint amount) external {
-        User storage staker = user[msg.sender];
-        uint totalStaked = staker.amountStaked;
-        require(totalStaked >= amount);
-        staker.amountStaked -= amount;
-        standardToken.transfer(msg.sender, amount);
+    function withdraw() public {
+        uint withdrawableBalance = user[msg.sender].amountStaked;
+        uint256 _stakeTime = user[msg.sender].timeStaked;
+        uint256 _stakeWithdrawalTime = _stakeTime + min_stake_duration;
+
+        if (_stakeWithdrawalTime > block.timestamp) {
+            revert("Not withdrawal timee yet");
+        } else {
+            require(withdrawableBalance > 0, "Insufficient staked balance");
+
+            uint256 calcStake = (withdrawableBalance * rate) / percentage;
+            uint256 stakeDuration = (block.timestamp - _stakeTime) /
+                min_stake_duration;
+            uint256 stakeReward = stakeDuration * calcStake;
+            standardToken.transfer(msg.sender, withdrawableBalance);
+            _mint(msg.sender, stakeReward);
+
+            contractBalance -= user[msg.sender].amountStaked;
+            user[msg.sender].amountStaked = 0;
+        }
     }
 }
